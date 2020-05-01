8e8548da8d05e59ff32372cab14e8bc305dbd911
lsb <leebutterman@gmail.com>
2020-05-01 15:16:29 -0700

    facemath

---
+++ leebutterman.com/facemath/precache-manifest.f698f9432bc9132de2ec14263d192fdc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08859129fa9883c05e125ed377487e4f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8fe4ec6eda9b5760241c",
+    "url": "./static/css/2.e61fc129.chunk.css"
+  },
+  {
+    "revision": "c489829c7435c563aadd",
+    "url": "./static/css/main.96957556.chunk.css"
+  },
+  {
+    "revision": "8fe4ec6eda9b5760241c",
+    "url": "./static/js/2.1ce12dd2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.1ce12dd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c489829c7435c563aadd",
+    "url": "./static/js/main.f08d9947.chunk.js"
+  },
+  {
+    "revision": "adfe59a88284f2875166",
+    "url": "./static/js/runtime-main.333f892b.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "./static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
