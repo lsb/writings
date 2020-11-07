8a0a4fa84db7b99f579405100ad5117e0d3edc83
lsb <leebutterman@gmail.com>
2020-11-07 12:31:50 -0800

    we have a checkout of l-p-t on the box, so, keep the symlinks

--- leebutterman.com/build.sh
+++ leebutterman.com/build.sh
@@ -1,1 +1,4 @@
-docker run -e JEKYLL_ENV=production -v $PWD:/jkl -w /jkl ljkl jekyll build
+rm /home/lsb/writings/leebutterman.com/light-pollution-topography
+docker run -e JEKYLL_ENV=production -v $PWD:/jkl -w /jkl -u $UID ljkl jekyll build
+ln -s /home/ubuntu/visualizing-light-pollution/light-pollution-topography/build/ /home/lsb/writings/leebutterman.com/light-pollution-topography
+ln -s /home/ubuntu/visualizing-light-pollution/light-pollution-topography/build/ /home/lsb/writings/leebutterman.com/_site/light-pollution-topography
