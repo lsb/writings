5c191ba5440b1b19028e321d4ef1225286b8cbb7
lsb <leebutterman@gmail.com>
2020-11-07 12:30:41 -0800

    verbose

--- watch-rebuild.sh
+++ watch-rebuild.sh
@@ -1,1 +1,1 @@
-pushd leebutterman.com/ ; docker run -e JEKYLL_ENV=production -v $PWD:/jkl -w /jkl ljkl jekyll build --watch --incremental ; popd
+pushd leebutterman.com/ ; docker run -e JEKYLL_ENV=production -v $PWD:/jkl -w /jkl ljkl jekyll build --watch --incremental --verbose ; popd
